7e2b4c727cde7078684f28cbae0bbdc366ece9e8
aiwaverse <aiwavision@protonmail.com>
2023-01-25 09:59:56 -0300

    wrote more typeinfer

--- lib/expr.ml
+++ lib/expr.ml
@@ -4,21 +4,14 @@ type expr =
   | Number of int
   | Boolean of bool
   | If of expr * expr * expr
-  | Function of expr * l1Type * expr
+  | Function of string * l1Type * expr
   | App of expr * expr
   | AppOp of operator * expr * expr
   | Let of expr * l1Type * expr
-  | Var of string * expr
+  | LetRec of expr * l1Type * expr
+  | Var of string
+  | Tuple of expr * expr
+  | Fst of expr
+  | Snd of expr
 
-and operator =
-  | Add of expr * expr
-  | Sub of expr * expr
-  | Mul of expr * expr
-  | Div of expr * expr
-  | LT of expr * expr
-  | LTE of expr * expr
-  | GT of expr * expr
-  | GTE of expr * expr
-  | EQ of expr * expr
-  | And of expr * expr
-  | Or of expr * expr
+and operator = Add | Sub | Mul | Div | LT | LTE | GT | GTE | EQ | And | Or
--- lib/typeinfer.ml
+++ lib/typeinfer.ml
@@ -3,13 +3,18 @@ open Types
 open Either
 module Ambient = Map.Make (String)
 
-let rec typeinfer (e : expr) (ambient : expr Ambient.t) =
+let rec typeinfer (e : expr) (ambient : l1Type Ambient.t) =
   match e with
   | Number _ -> Right Int
   | Boolean _ -> Right Bool
-  | If (cond, e1, e2) -> typeinferIf (If (cond, e1, e2)) ambient
+  | If _ as e -> typeinferIf e ambient
+  | Tuple _ as e -> typeinferTuple e ambient
+  | Function _ as e -> typeinferFunction e ambient
+  | Var _ as e -> typeinferVar e ambient
+  | AppOp _ as e -> typeinferAppOp e ambient
+  | _ -> Left "Not Implemented"
 
-and typeinferIf (e : expr) (ambient : expr Ambient.t) =
+and typeinferIf (e : expr) (ambient : l1Type Ambient.t) =
   match e with
   | If (cond, e1, e2) -> (
       match typeinfer cond ambient with
@@ -18,11 +23,12 @@ and typeinferIf (e : expr) (ambient : expr Ambient.t) =
           | Right ee1 -> (
               match typeinfer e2 ambient with
               | Right ee2 ->
-                  if ee1 = ee2 then Right ee1
+                  if ee1 == ee2 then Right ee1
                   else
                     Left
-                      ("typeinfer failed on If, expected " ^ print_l1Type ee1
-                     ^ ", found " ^ print_l1Type ee2)
+                      ("typeinfer failed on If, e1 and e2 types are different, \
+                        expected " ^ print_l1Type ee1 ^ ", found "
+                     ^ print_l1Type ee2)
               | Left err ->
                   Left ("typeinferIf failed on second argument\n" ^ err))
           | Left err -> Left ("typeinferInf failed on first argument\n" ^ err))
@@ -30,3 +36,86 @@ and typeinferIf (e : expr) (ambient : expr Ambient.t) =
           Left ("typeinfer failed on If, expected Bool, found " ^ print_l1Type e)
       | Left err -> Left ("typeinferInf failed on condition\n" ^ err))
   | _ -> Left "typeinferIf used on something that isn't an if"
+
+and typeinferTuple (e : expr) (ambient : l1Type Ambient.t) =
+  match e with
+  | Tuple (e1, e2) -> (
+      match typeinfer e1 ambient with
+      | Right t1 -> (
+          match typeinfer e2 ambient with
+          | Right t2 -> Right (Pair (t1, t2))
+          | Left err2 ->
+              Left ("typeinferTuple failed on second argument\n" ^ err2))
+      | Left err1 -> Left ("typeinferTuple failed on first argument\n" ^ err1))
+  | _ -> Left "typeinferTuple used on something that isn't a tuple"
+
+and typeinferVar (e : expr) (ambient : l1Type Ambient.t) =
+  match e with
+  | Var (name) -> (
+      match Ambient.find_opt name ambient with
+      | Some t -> Right t
+      | None -> Left ("Variable " ^ name ^ " not found on ambient"))
+  | _ -> Left "typeinferVar used on something that isn't a var"
+
+and typeinferFunction (e : expr) (ambient : l1Type Ambient.t) =
+  match e with
+  | Function (varName, varType, body) -> (
+      match typeinfer body (Ambient.add varName varType ambient) with
+      | Right bodyType -> Right (Func (varType, bodyType))
+      | Left err -> Left ("typeinfer failed on Function\n" ^ err))
+  | _ -> Left "typeinferFunction used on something that isn't a function"
+
+and typeinferAppOp (e : expr) (ambient : l1Type Ambient.t) =
+  match e with
+  | AppOp (op, e1, e2) -> (
+      match op with
+      | Add | Sub | Mul | Div -> (
+          match typeinfer e1 ambient with
+          | Right Int -> (
+              match typeinfer e2 ambient with
+              | Right Int -> Right Int
+              | Right t ->
+                  Left
+                    ("typeinferAppOp failed on second argument, expected Int, \
+                      found " ^ print_l1Type t)
+              | Left err -> Left ("typeinfer failed on second argument\n" ^ err)
+              )
+          | Right t ->
+              Left
+                ("typeinferAppOp failed on first argument, expected Int, found "
+               ^ print_l1Type t)
+          | Left err -> Left ("typeinfer failed on first argument\n" ^ err))
+      | LT | LTE | GT | GTE | EQ -> (
+          match typeinfer e1 ambient with
+          | Right Int -> (
+              match typeinfer e2 ambient with
+              | Right Int -> Right Bool
+              | Right t ->
+                  Left
+                    ("typeinferAppOp failed on second argument, expected Int, \
+                      found " ^ print_l1Type t)
+              | Left err -> Left ("typeinfer failed on second argument\n" ^ err)
+              )
+          | Right t ->
+              Left
+                ("typeinferAppOp failed on first argument, expected Int, found "
+               ^ print_l1Type t)
+          | Left err -> Left ("typeinfer failed on first argument\n" ^ err))
+      | And | Or -> (
+          match typeinfer e1 ambient with
+          | Right Bool -> (
+              match typeinfer e2 ambient with
+              | Right Bool -> Right Bool
+              | Right t ->
+                  Left
+                    ("typeinferAppOp failed on second argument, expected Int, \
+                      found " ^ print_l1Type t)
+              | Left err -> Left ("typeinfer failed on second argument\n" ^ err)
+              )
+          | Right t ->
+              Left
+                ("typeinferAppOp failed on first argument, expected Int, found "
+               ^ print_l1Type t)
+          | Left err -> Left ("typeinfer failed on first argument\n" ^ err)))
+  | _ ->
+      Left "typeinferAppOp used on something that isn't an operator application"
