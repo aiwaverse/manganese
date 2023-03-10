ca85375a32302eaa75348d3f05f16b02bb8de76d
Gustavo <gusbgomes@gmail.com>
2023-03-10 01:23:59 -0300

    qq

--- lib/expr.ml
+++ lib/expr.ml
@@ -1,6 +1,6 @@
 open Types
 
-type expr =
+type expr = 
   | Number of int
   | Boolean of bool
   | If of expr * expr * expr
@@ -26,3 +26,59 @@ type expr =
   | FromJust of expr
 
 and operator = Add | Sub | Mul | Div | LT | LTE | GT | GTE | EQ | And | Or
+
+let print_op = function
+  | Add -> "+"
+  | Sub -> "-"
+  | Mul -> "*"
+  | Div -> "/"
+  | LT  -> "<"
+  | LTE -> "<="
+  | GT  -> ">"
+  | GTE -> ">="
+  | EQ  -> "=="
+  | And -> "&&"
+  | Or  -> "||"
+
+let rec print_expr = function
+  | Number n -> string_of_int n
+  | Boolean b -> string_of_bool b
+  | If(b, e1, e2) -> "If " ^ print_expr b ^ " then " ^ print_expr e1 ^ " else " ^ print_expr e2
+  | Function(varName, varType, body) -> "fn " ^ varName ^ ":" ^ print_l1Type varType ^ " => " ^ print_expr body
+  | App(e1, e2) -> print_expr e1 ^ " @ " ^ print_expr e2
+  | AppOp(op, e1, e2) -> print_expr e1 ^ " " ^ print_op op ^ " " ^ print_expr e2
+  | Let(varName, varType, varDefinition, body) -> "Let " ^ varName ^ ":" ^ print_l1Type varType ^ " => " ^ print_expr varDefinition ^ " in " ^ print_expr body
+  | LetRec(fName, t1, t2, innerFName, e1, e2) -> 
+      "Let Rec " ^ 
+      fName ^ ":" ^
+      print_l1Type t1 ^ " -> " ^
+      print_l1Type t2 ^ " = (fn " ^
+      innerFName ^ ":" ^
+      print_l1Type t1 ^ " => " ^
+      print_expr e1 ^ ") in " ^
+      print_expr e2
+  | Var name -> name
+  | Tuple(first, second) -> "(" ^ print_expr first ^ ", " ^ print_expr second ^ ")"
+  | Fst e -> "fst " ^ print_expr e
+  | Snd e -> "snd " ^ print_expr e
+  | Nil t -> "nil:" ^ print_l1Type t
+  | Cons(e1, e2) -> print_expr e1 ^ " : " ^ print_expr e2
+  | Head e -> "hd " ^ print_expr e
+  | Tail e -> "tl " ^ print_expr e
+  | MatchList(e1, e2, x, xs, e3) ->
+      "match " ^
+      print_expr e1 ^ " with nil => " ^
+      print_expr e2 ^ " | " ^
+      x ^ " :: " ^ xs ^ " => " ^
+      print_expr e3
+  | Just e -> "just " ^ print_expr e
+  | Nothing t -> "nothing:" ^print_l1Type t
+  | MatchMaybe(e1, e2, x, e3) ->
+    "match " ^
+      print_expr e1 ^ " with nil => " ^
+      print_expr e2 ^ " | " ^
+      "just " ^ x ^ " => " ^
+      print_expr e3
+  | IsEmpty e -> "isEmpty " ^ print_expr e
+  | IsNothing e -> "isNothing " ^ print_expr e
+  | FromJust e -> "fromJust " ^ print_expr e
---
+++ test/test_eval.ml
@@ -0,0 +1,299 @@
+open Manganese.Expr
+open Manganese.Eval
+open Manganese.Types
+open Either
+open OUnit2
+
+let simpleTests =
+  [
+    ( "simple number" >:: fun _ ->
+      assert_equal (Right (Number 2)) (eval (Number 2)) );
+    ( "simple boolean - true" >:: fun _ ->
+      assert_equal (Right (Boolean true)) (eval (Boolean true)) );
+    ( "simple boolean - false" >:: fun _ ->
+      assert_equal (Right (Boolean false)) (eval (Boolean false)) );
+    ( "function" >:: fun _ ->
+      assert_equal
+        (Right (Function ("x", Bool, If (Var "x", Boolean true, Boolean false))))
+        (eval (Function ("x", Bool, If (Var "x", Boolean true, Boolean false)))) );
+    ( "let rec" >:: fun _ ->
+      assert_equal (Right (Number 120))
+        (eval
+           (LetRec
+              ( "fat",
+                Int,
+                Int,
+                "x",
+                If
+                  ( AppOp (EQ, Var "x", Number 0),
+                    Number 1,
+                    AppOp
+                      ( Mul,
+                        Var "x",
+                        App (Var "fat", AppOp (Sub, Var "x", Number 1)) ) ),
+                App (Var "fat", Number 5) ))) );
+  ]
+
+let ifTests =
+  [
+    ( "if - well written" >:: fun _ ->
+      assert_equal (Right (Boolean true))
+        (eval (If (Boolean true, Boolean true, Boolean false))) );
+    ( "if - poorly written, cond isn't boolean" >:: fun _ ->
+       assert_bool "Failed for if"
+        (is_left (eval (If (Number 0, Boolean true, Boolean false)))) );
+    ( "if - poorly written, cond poorly written" >:: fun _ ->
+       assert_bool "Failed for if"
+        (is_left (eval (If (If (Number 0, Boolean true, Boolean false), Boolean true, Boolean false)))) );
+  ]
+
+let tupleTests =
+  [
+    ( "tuple - well written" >:: fun _ ->
+      assert_equal
+        (Right (Tuple (Number 0, Boolean true)))
+        (eval (Tuple (Number 0, Boolean true))) );
+    ( "tuple - poorly written, first term is poorly written" >:: fun _ ->
+      assert_bool "Failed for tuple"
+        (is_left
+           (eval
+              (Tuple (If (Number 2, Number 1, Boolean false), Number 2))))
+    );
+    ( "tuple - poorly written, second term is poorly written" >:: fun _ ->
+      assert_bool "Failed for tuple"
+        (is_left
+           (eval
+              (Tuple (Number 2, If (Number 2, Number 1, Boolean false)))))
+    );
+  ]
+
+let varTests =
+  [
+    ( "var - in ambient" >:: fun _ ->
+      assert_equal (Right (Number 0))
+        (eval (Var "x") ~ambient:(Ambient.add "x" (Number 0) Ambient.empty)) );
+    ( "var - not in ambient" >:: fun _ ->
+      assert_bool "Failed for var"
+        (is_left
+           (eval (Var "x") ~ambient:(Ambient.add "y" (Number 0) Ambient.empty)))
+    );
+  ]
+
+let appTests =
+  [
+    ( "application - well written" >:: fun _ ->
+      assert_equal (Right (Boolean true))
+        (eval
+           (App
+              ( Function ("x", Bool, If (Var "x", Boolean true, Boolean false)),
+                Boolean true ))
+                ~ambient:Ambient.empty) );
+    ( "application - poorly written, first term isn't a function" >:: fun _ ->
+      assert_bool "failed on application"
+        (is_left (eval (App (Boolean false, Boolean true)))) );
+  ]
+
+let appOpTests =
+  [
+    ( "operator application, add - well written" >:: fun _ ->
+      assert_equal (Right (Number 3)) (eval (AppOp (Add, Number 1, Number 2))) );
+    ( "operator application, add - poorly written, first expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Add, Boolean false, Number 2)))) );
+    ( "operator application, add - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Add, Number 1, Boolean false)))) );
+    ( "operator application, sub - well written" >:: fun _ ->
+      assert_equal (Right (Number (-1))) (eval (AppOp (Sub, Number 1, Number 2))) );
+    ( "operator application, sub - poorly written, first expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Sub, Boolean false, Number 2)))) );
+    ( "operator application, sub - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Sub, Number 1, Boolean false)))) );
+    ( "operator application, mul - well written" >:: fun _ ->
+      assert_equal (Right (Number 2)) (eval (AppOp (Mul, Number 1, Number 2))) );
+    ( "operator application, mul - poorly written, first expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Mul, Boolean false, Number 2)))) );
+    ( "operator application, mul - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Mul, Number 1, Boolean false)))) );
+    ( "operator application, div - well written" >:: fun _ ->
+      assert_equal (Right (Number 0)) (eval (AppOp (Div, Number 1, Number 2))) );
+    ( "operator application, div - poorly written, first expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Div, Boolean false, Number 2)))) );
+    ( "operator application, div - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Div, Number 1, Boolean false)))) );
+    ( "operator application, div - poorly written, second expression evaluates to zero"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Div, Number 1, Number 0)))) );
+    ( "operator application, LT - well written" >:: fun _ ->
+      assert_equal (Right (Boolean true)) (eval (AppOp (LT, Number 1, Number 2))) );
+    ( "operator application, LT - poorly written, first expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (LT, Boolean false, Number 2)))) );
+    ( "operator application, LT - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (LT, Number 1, Boolean false)))) );
+    ( "operator application, LTE - well written" >:: fun _ ->
+      assert_equal (Right (Boolean true)) (eval (AppOp (LTE, Number 1, Number 2))) );
+    ( "operator application, LTE - poorly written, first expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (LTE, Boolean false, Number 2)))) );
+    ( "operator application, LTE - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (LTE, Number 1, Boolean false)))) );
+    ( "operator application, GT - well written" >:: fun _ ->
+      assert_equal (Right (Boolean false)) (eval (AppOp (GT, Number 1, Number 2))) );
+    ( "operator application, GT - poorly written, first expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (GT, Boolean false, Number 2)))) );
+    ( "operator application, GT - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (GT, Number 1, Boolean false)))) );
+    ( "operator application, GTE - well written" >:: fun _ ->
+      assert_equal (Right (Boolean false)) (eval (AppOp (GTE, Number 1, Number 2))) );
+    ( "operator application, GTE - poorly written, first expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (GTE, Boolean false, Number 2)))) );
+    ( "operator application, GTE - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (GTE, Number 1, Boolean false)))) );
+    ( "operator application, EQ - well written" >:: fun _ ->
+      assert_equal (Right (Boolean false)) (eval (AppOp (EQ, Number 1, Number 2))) );
+    ( "operator application, EQ - poorly written, first expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (EQ, Boolean false, Number 2)))) );
+    ( "operator application, EQ - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (EQ, Number 1, Boolean false)))) );
+    ( "operator application, And - well written" >:: fun _ ->
+      assert_equal (Right (Boolean true))
+        (eval (AppOp (And, Boolean true, Boolean true))) );
+    ( "operator application, And - poorly written, first expression isn't boolean"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (And, Number 1, Boolean false)))) );
+    ( "operator application, And - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (And, Boolean false, Number 1)))) );
+    ( "operator application, Or - well written" >:: fun _ ->
+      assert_equal (Right (Boolean true))
+        (eval (AppOp (Or, Boolean true, Boolean false))) );
+    ( "operator application, Or - poorly written, first expression isn't boolean"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Or, Number 1, Boolean false)))) );
+    ( "operator application, Or - poorly written, second expression isn't number"
+    >:: fun _ ->
+      assert_bool "failed on applcation operator"
+        (is_left (eval (AppOp (Or, Boolean false, Number 1)))) );
+  ]
+
+let letTests =
+  [
+    ( "let - well written" >:: fun _ ->
+      assert_equal (Right (Number 3))
+        (eval (Let ("x", Int, Number 1, AppOp (Add, Var "x", Number 2))))
+    );
+    ( "let - poorly written, body poorly written" >:: fun _ ->
+      assert_bool "failed on let"
+        (is_left
+           (eval
+              (Let ("x", Int, Number 1, AppOp (Add, Var "y", Number 2))))) );
+  ]
+
+let fstTests =
+  [
+    ( "fst - well written" >:: fun _ ->
+      assert_equal (Right (Number 1))
+        (eval (Fst (Tuple (Number 1, Boolean false)))) );
+    ( "fst - poorly written" >:: fun _ ->
+      assert_bool "failed on fst"
+        (is_left (eval (Fst (Tuple (Var "x", Boolean false))))) );
+  ]
+
+let sndTests =
+  [
+    ( "snd - well written" >:: fun _ ->
+      assert_equal (Right (Boolean false))
+        (eval (Snd (Tuple (Number 1, Boolean false)))) );
+    ( "snd - poorly written" >:: fun _ ->
+      assert_bool "failed on snd"
+        (is_left (eval(Snd (Tuple (Var "x", Boolean false))))) );
+  ]
+
+let nilTests =
+  [
+    ( "nil - well written" >:: fun _ ->
+      assert_equal (Right (Nil Int)) (eval (Nil Int)) );
+  ]
+
+let consTests =
+  [
+    ( "cons - well written" >:: fun _ ->
+      assert_equal (Right (Cons (Number 1, Nil Int))) (eval (Cons (Number 1, Nil Int))) );
+    ( "cons - poorly written" >:: fun _ ->
+      assert_bool "failed on cons"
+        (is_left (eval (Cons (Var "x", Nil Int)))) );
+  ]
+
+let headTests =
+  [
+    ( "head - well typed" >:: fun _ ->
+      assert_equal (Right (Number 1)) (eval (Head (Cons (Number 1, Nil Int)))) );
+    ( "head - poorly typed, argument is an empty list" >:: fun _ ->
+      assert_bool "failed on head" (is_left (eval (Head (Nil Int)))) );
+    ( "head - poorly typed, argument isn't a list" >:: fun _ ->
+      assert_bool "failed on head" (is_left (eval (Head (Number 1)))) );
+    ( "head - poorly typed, argument is poorly typed" >:: fun _ ->
+      assert_bool "failed on head"
+        (is_left (eval (Head (Cons (Var "x", Nil Int))))) );
+  ]
+
+  let tailTests =
+    [
+      ( "tail - well typed" >:: fun _ ->
+        assert_equal (Right (Nil Int))
+          (eval (Tail (Cons (Number 1, Nil Int)))) );
+      ( "tail - poorly typed, argument is an empty list" >:: fun _ ->
+        assert_bool "failed on tail" (is_left (eval (Tail (Nil Int)))) );
+      ( "tail - poorly typed, argument isn't a list" >:: fun _ ->
+        assert_bool "failed on tail" (is_left (eval (Tail (Number 1)))) );
+      ( "tail - poorly typed, argument is poorly typed" >:: fun _ ->
+        assert_bool "failed on tail"
+          (is_left (eval (Tail (Cons (Var "x", Nil Int))))) );
+    ]
+
+    let tests =
+      "test suite for eval"
+      >::: simpleTests @ ifTests @ tupleTests @ varTests @ appTests
+           @ appOpTests @ letTests @ fstTests @ sndTests @ nilTests
+           @ consTests @ headTests @ tailTests (*@ nothingTests @ justTests
+           @ matchListTests @ matchMaybeTests @ isEmptyTests @ isNothingTests
+           @ fromJustTests*)
+    
+    let _ = run_test_tt_main tests
